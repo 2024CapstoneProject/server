ef64c6f3e5dbd4f782dafbb7c918c62b99412c67
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-03-25 16:25:27 +0900

    openAPI whisper 사용

---
+++ src/main/java/com/example/kioskhelper/controller/AudioController.java
@@ -0,0 +1,30 @@
+package com.example.kioskhelper.controller;
+
+
+import com.example.kioskhelper.domain.dto.TranscriptionRequest;
+import com.example.kioskhelper.domain.dto.WhisperTranscriptionResponse;
+import com.example.kioskhelper.service.OpenAIClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api")
+public class AudioController {
+
+private final OpenAIClientService openAIClientService;
+
+
+    @PostMapping(value = "/transcription", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public WhisperTranscriptionResponse createTranscription(@ModelAttribute TranscriptionRequest transcriptionRequest){
+        return openAIClientService.createTranscription(transcriptionRequest);
+    }
+
+
+
+}

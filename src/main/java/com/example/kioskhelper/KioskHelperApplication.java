5e7644631e0750f1308482975a139cca22def273
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-03-25 14:26:45 +0900

    초기 project setting

---
+++ src/main/java/com/example/kioskhelper/KioskHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.kioskhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskHelperApplication.class, args);
+    }
+
+}
